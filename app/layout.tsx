4314584032b631abf3b6f181c12973e4aa3d3f35
Fernando Esdras da Silva <contatoesdrasoficial@gmail.com>
2024-05-23 04:57:38 -0300

    example favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Header } from "./components/header/header";
 import { Cursor } from "./utils/cursor/cursor";
 import { ProviderPreferences } from "./contexts/ContextPreferences";
 import { TransitionPage } from "./components/transition-page/transition-page";
-import favicon from "../public/favicon1.ico";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -27,7 +26,7 @@ export default function RootLayout({
           />
           <meta property="og:locale" content="pt-BR" />
           <meta name="theme-color" content="#161616" />
-          <link rel="icon" href={`${favicon}`} />
+          <link rel="icon" href="/favicon1.ico" />
           <title>Esdras Dev</title>
           <meta property="og:title" content="Esdras Dev" />
           <meta property="og:site_name" content="Esdras Dev" />
@@ -48,7 +47,7 @@ export default function RootLayout({
             name="keywords"
             content="Fernando Esdras da Sivca, Esdras, Silva, programador, fullstack, dev, desenvolvedor, São Paulo, SP, Brasil, Brazil, América, React, Next, React.js, Next.js, HTML5, CSS3 landing page, ecommerce, site, freela, freelancer, melhor, melhores, perto de mim, Node, Backend, Frontend, Fullstack, Nest, SQL, NOSQL, MySql, Javascript, Typescript, React NAtive, Docker, AWS, Azure, Cloud, Api"
           />
-          <meta property="og:image" content={`${favicon}`} />
+          <meta property="og:image" content="/favicon1.ico" />
         </head>
         <body
           className={`${inter.className} overflow-x-hidden bg-primary text-primary duration-300 transition-all ease-linear scroll-styled`}
